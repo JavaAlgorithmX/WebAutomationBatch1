342d46bb0d60ae290d1b2385f44fd064f9c227e0
madhavSharma23 <mastermadhav001@gmail.com>
2024-01-05 23:50:47 +0530

    first commit

---
+++ src/main/java/com/brainonetech/day1/SeleniumIntro.java
@@ -0,0 +1,10 @@
+package com.brainonetech.day1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumIntro {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+    }
+}
